716714cc36e22f245e58d65ab8bab1140a8f3ef2
wipdev-tech <wipdev.tech@gmail.com>
2024-01-19 20:11:56 +0000

    First commit with working config

---
+++ .wezterm.lua
@@ -0,0 +1,28 @@
+local wezterm = require 'wezterm'
+local config = {}
+
+if wezterm.config_builder then
+    config = wezterm.config_builder()
+end
+
+config.color_scheme = 'catppuccin-mocha'
+config.window_decorations = "NONE"
+config.enable_tab_bar = false
+config.font = wezterm.font('FiraMono Nerd Font Mono')
+config.font_size = 12.0
+config.line_height = 1.1
+config.keys = {
+    {
+        key = 'F11',
+        action = wezterm.action.ToggleFullScreen,
+    },
+}
+config.window_padding = {
+  left = 4,
+  right = 4,
+  top = 4,
+  bottom = 4,
+}
+
+
+return config
